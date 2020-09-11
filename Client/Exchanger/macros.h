afebf7a600cc041a0b5470167165ccfd59156450
Suyash Chavan <57249574+suyashc222@users.noreply.github.com>
2020-09-11 23:17:58 +0530

    Update macros.h

--- Client/Exchanger/macros.h
+++ Client/Exchanger/macros.h
@@ -1,7 +1,9 @@
 #if defined (_WIN32) || defined (_WIN64)
  #define Local_Database ""
  #define Bridge_File ""
+ #define File_Transfer ""
 #elif defined (__unix__)
  #define Local_Database ""
  #define Bridge_File ""
+ #define File_Transfer ""
 #endif
