574e0839122968f452bad811d4a77d03c35bbc52
Suyash Chavan <57249574+suyashc222@users.noreply.github.com>
2020-09-11 23:31:51 +0530

    Update features.h

--- Client/Exchanger/features.h
+++ Client/Exchanger/features.h
@@ -1,3 +1,5 @@
+#include <macros.h>
+
 void run_script(struct _message message)
 {
 
@@ -5,10 +7,15 @@ void run_script(struct _message message)
 
 void file_transfer(struct _message message)
 {
-
+  /*
+    File Transfer Location at File_Transfer.
+  */
 }
 
 void loopback_handler(struct _message message)
 {
-
+  /*
+    Local Database location at Local_Database.
+    Bridge file location at Bridge_File.
+  */
 }
