598c0533de4768d7c0276c3aba3a8e74a75260d6
Suyash Chavan <57249574+suyashc222@users.noreply.github.com>
2020-10-15 16:31:47 +0530

    Update and rename new file to core.cpp

---
+++ Server/core.cpp
@@ -0,0 +1,31 @@
+#include <bits/stdc++.h>
+#include "map_structs.h"
+using namespace std;
+
+map<KEY, VALUE*> Database;
+
+void ClientInit(struct _message something)
+{
+  KEY ClientInfo;
+  ClientInfo.IP=IP;
+  ClientInfo.PORT=PORT;
+  
+  Database[ClientInfo] = new VALUE;
+
+  Database[ClientInfo]->TIMESTAMP.DATE=DATE;
+  Database[ClientInfo]->TIMESTAMP.TIME=TIME;
+  
+}
+
+void UserInit(struct _message something)
+{
+  // User Info In map
+}
+
+int main()
+{
+
+
+ 
+  return 0;
+}
