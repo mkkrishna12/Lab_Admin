940fad764b4f2849b5fc28aa1e5d642c964175ec
Suyash Chavan <57249574+suyashc222@users.noreply.github.com>
2020-10-15 16:32:51 +0530

    Create dataset.h

---
+++ Server/dataset.h
@@ -0,0 +1,22 @@
+struct _timeStamp{
+  string DATE;
+  string TIME;
+};
+
+struct _user{
+  string YEAR;
+  string BRANCH;
+  string PRN;
+  string NAME;
+  struct _timeStamp TIMESTAMP;
+};
+
+typedef struct _key{
+  string IP;
+  string PORT;
+} KEY;
+
+typedef struct _value{
+  struct _user* USER_DATA;
+  struct _timeStamp TIMESTAMP;
+} VALUE;
