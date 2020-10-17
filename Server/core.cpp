38b05d3ccb9a8ac23828d664a78096e578aef672
kajalp23 <64576673+kajalp23@users.noreply.github.com>
2020-10-17 21:10:01 +0530

    Update core.cpp

--- Server/core.cpp
+++ Server/core.cpp
@@ -8,25 +8,36 @@ map<KEY, VALUE*> Database;
 void ClientInit(struct _message something)
 {
   KEY ClientInfo;
-  ClientInfo.IP=IP;
-  ClientInfo.PORT=PORT;
-  
+  ClientInfo.IP = something.message_data.ip;
+  ClientInfo.PORT = something.message_data.port;
+
   Database[ClientInfo] = new VALUE;
 
-  Database[ClientInfo]->TIMESTAMP.DATE=DATE;
-  Database[ClientInfo]->TIMESTAMP.TIME=TIME;
-  
+  Database[ClientInfo]->TIMESTAMP.DATE = something.message_data.data.msg.user_info.DATE;
+  Database[ClientInfo]->TIMESTAMP.TIME = something.message_data.data.msg.user_info.TIME;
+
 }
 
 void UserInit(struct _message something)
 {
-  // User Info In map
+  KEY ClientInfo;
+  ClientInfo.IP=something.message_data.ip;
+  ClientInfo.PORT=something.message_data.port;
+
+  Database[ClientInfo] = new VALUE;
+
+  Database[ClientInfo]->USER_DATA->TIMESTAMP.DATE = something.message_data.data.msg.user_info.DATE;
+  Database[ClientInfo]->USER_DATA->TIMESTAMP.TIME = something.message_data.data.msg.user_info.TIME;
+  Database[ClientInfo]->USER_DATA->YEAR = something.message_data.data.msg.user_info.YEAR;
+  Database[ClientInfo]->USER_DATA->BRANCH = something.message_data.data.msg.user_info.BRANCH;
+  Database[ClientInfo]->USER_DATA->PRN = something.message_data.data.msg.user_info.PRN;
+  Database[ClientInfo]->USER_DATA->NAME = something.message_data.data.msg.user_info.NAME;
 }
 
 int main()
 {
 
 
- 
+
   return 0;
 }
